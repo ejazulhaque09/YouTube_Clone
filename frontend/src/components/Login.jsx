6c630606ea7f5f3f5768f5a765cf90452353dab5
ejazulhaque0 <ejazulhaque09@gmail.com>
2024-11-29 00:09:36 +0530

    Integrated frontend with backend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import LinearProgress from "@mui/material/LinearProgress";
 import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
 const Login = ({setLoginModal}) => {
     const [loader, setLoader] = useState(false);
     const [loginField, setLoginField] = useState({
@@ -16,6 +17,29 @@ const Login = ({setLoginModal}) => {
             [name]: e.target.value,
         })
     }
+
+    const handleLoginFunc = async () => {
+        setLoader(true);
+        console.log("calling api")
+        console.log(loginField)
+        axios.
+        post('http://localhost:4000/auth/login', loginField, {
+            withCredentials: true
+        })
+        .then((res) => {
+            console.log("Api called")
+            console.log(res)
+            setLoader(false);
+            localStorage.setItem("token", res.data.token);
+            localStorage.setItem("userId", res.data.user._id);
+            localStorage.setItem("profilePic", res.data.user.profilePic);
+            window.location.reload();
+        }).catch((err) => {
+            toast.error("Invalid Credential")
+            console.log(err)
+            setLoader(false);
+        })
+    }
     
     return(
         <div className="fixed inset-0 w-full h-full bg-white bg-opacity-80 flex justify-center items-center text-black">
@@ -26,21 +50,21 @@ const Login = ({setLoginModal}) => {
                 </div>
                 <div className="flex flex-col gap-8 mt-6 w-full items-center">
                     <div className="userNameLogin">
-                        <input type="text" className="w-auto h-12 text-black px-3 bg-gray-200 border-none rounded-md placeholder:text-gray-600"  placeholder="UserName" onChange={(e) => handleOnChangeInput(e, 'userName')}/>
+                        <input type="text" className="w-auto h-12 text-black px-3 bg-gray-200 border-none rounded-md placeholder:text-gray-600"  placeholder="UserName" value={loginField.userName} onChange={(e) => handleOnChangeInput(e, 'userName')}/>
                     </div>
                     <div className="userNameLogin">
-                        <input type="password" className="w-auto h-12 text-black px-3 bg-gray-200 border-none rounded-md placeholder:text-gray-600" placeholder="Password" onChange={(e) => handleOnChangeInput(e, 'password')}/>
+                        <input type="password" className="w-auto h-12 text-black px-3 bg-gray-200 border-none rounded-md placeholder:text-gray-600" placeholder="Password" value={loginField.password} onChange={(e) => handleOnChangeInput(e, 'password')}/>
                     </div>
                 </div>
                 <div className="w-3/5 flex justify-between mt-6">
-                    <div className=" w-auto px-2 h-9 border text-center flex justify-center items-center rounded-md text-lg font-semibold hover:text-black bg-gray-200 border-black cursor-pointer  hover:bg-gray-400">
+                    <div onClick={handleLoginFunc} className=" w-auto px-2 h-9 border text-center flex justify-center items-center rounded-md text-lg font-semibold hover:text-black bg-gray-200 border-black cursor-pointer  hover:bg-gray-400">
                         Login
                     </div>
                     <Link to={'/signup'} className='w-auto px-2 mx-4 h-9 border text-center flex justify-center items-center rounded-md text-lg font-semibold hover:text-black bg-gray-200 border-black cursor-pointer  hover:bg-gray-400' onClick={() => setLoginModal()}> SignUp</Link>
                     <div className=" w-auto px-2 h-9 border text-center flex justify-center items-center rounded-md text-lg font-semibold hover:text-black bg-gray-200 border-black cursor-pointer  hover:bg-gray-400" onClick={() => setLoginModal()}> Cancel</div>
                 </div>
                 {loader && (
-                <Box sx={{width: '100%'}}>
+                <Box sx={{width: '100%' }}>
                     <LinearProgress/>
                 </Box>
                 )}
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,65 +1,125 @@
-import React from "react";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
+import React, { useEffect, useState } from "react";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SideNavbar from "../components/SideNavbar";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const Profile = ({ sideNavBar }) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(null);
+  const fetchProfileData = async () => {
+    axios
+      .get(`http://localhost:4000/video/${id}/channel`)
+      .then((res) => {
+        console.log(res);
+        setData(res.data.data);
+        setUser(res.data.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
+  const handlEdit = (videoId) => {
+    navigate(`//${videoId}/edit`);
+  };
+  const handleDelete = async (videoId) => {
+    try {
+      await axios.delete(`http://localhost:4000/video/video/${videoId}`, {
+        withCredentials: true,
+      });
+      setData(data.filter((video) => video._id !== video));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const toggleMenu = (index) => {
+    setMenuOpen(menuOpen === index ? null : index);
+  };
   return (
-  <div className="flex w-full p-2.5 box-border bg-white text-black">
-    <SideNavbar sideNavbar={sideNavBar} />
-    <h2>nav</h2>
-    <div className={sideNavBar ? "flex flex-col overflow-x-hidden flex-1 ml-[270px] mt-[56px] text-black justify-center items-center" : "flex flex-col overflow-x-hidden flex-1 text-black mt-14 justify-center items-center"}>
-      <div className="w-full flex pb-5">
-        <div className="w-[150px] h-[150px]">
-          <img
-            src="img.jpg"
-            alt="profilePic"
-            className="w-full h-full px-2.5"
-          />
-        </div>
-        <div className="flex flex-col gap-2 w-[85%] px-2.5">
-          <div className="text-4xl font-semibold">Channel Name</div>
-          <div className="text-sm text-gray-500">
-            Channel name . 5 Videos
+    <div className="flex w-full p-2.5 box-border bg-white text-black">
+      <SideNavbar sideNavbar={sideNavBar} />
+      <h2>nav</h2>
+      <div
+        className={
+          sideNavBar
+            ? "flex flex-col overflow-x-hidden flex-1 ml-[270px] mt-[56px] text-black justify-center items-center"
+            : "flex flex-col overflow-x-hidden flex-1 text-black mt-14 justify-center items-center"
+        }
+      >
+        <div className="w-full flex pb-5">
+          <div className="w-[150px] h-[150px]">
+            <img
+              src={user?.profilePic}
+              alt="profilePic"
+              className="w-full h-full px-2.5"
+            />
           </div>
-          <div className="text-sm text-gray-500">About</div>
-        </div>
-      </div>
-      <div className="profile_videos w-full">
-        <div className="text-lg pb-2.5 text-gray-300 font-medium flex items-center border-b border-gray-600">
-          Videos &nbsp; <ArrowRightIcon />
-        </div>
-        <div className="flex gap-2.5 h-screen flex-wrap mt-5">
-          {/* Static data */}
-          <div>
-            <div className="w-[210px] text-black cursor-pointer no-underline">
-              <div className="w-full h-[140px]">
-                <img
-                  src="img.jpg"
-                  alt="thumbnail"
-                  className="w-full h-full"
-                />
-              </div>
-              <div className="flex flex-col w-full">
-                <div className="w-full text-base font-semibold">Title</div>
-                <div className="text-sm text-gray-400">
-                  Created at 12-02-2024
-                </div>
-              </div>
+          <div className="flex flex-col gap-2 w-[85%] px-2.5">
+            <div className="text-4xl font-semibold">{user?.channelName}</div>
+            <div className="text-sm text-gray-500">
+              {console.log(data)}
+              {user?.channelName} . {data?.length} videos
             </div>
-            <div className="profileVideo_block_menu">
-                <MoreVertIcon/>
-                {/* Static data */}
-                <div className="profileVideo_block_menu_options">
-                    <button className="profileVideo_block_menu_option">Edit</button>
-                    <button className="profileVideo_block_menu_option">Delete</button>
+            <div className="text-sm text-gray-500">{user?.about}</div>
+          </div>
+        </div>
+        <div className="profile_videos w-full">
+          <div className="text-lg pb-2.5 text-gray-300 font-medium flex items-center border-b border-gray-600">
+            Videos &nbsp; <ArrowRightIcon />
+          </div>
+          <div className="flex gap-2.5 h-screen flex-wrap mt-5">
+            {data.map((item, index) => {
+              return (
+                <div>
+                  <Link to={`/watch/${item._id}`} className="w-[210px] text-black cursor-pointer no-underline">
+                    <div className="w-full h-[140px]">
+                      <img
+                        src="img.jpg"
+                        alt="thumbnail"
+                        className="w-full h-full"
+                      />
+                    </div>
+                    <div className="flex flex-col w-full">
+                      <div className="w-full text-base font-semibold">
+                        Title
+                      </div>
+                      <div className="text-sm text-gray-400">
+                        Created at 12-02-2024
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="profileVideo_block_menu">
+                    <MoreVertIcon onClick={() => toggleMenu(index)} />
+                    {/* Static data */}
+                    <div className="profileVideo_block_menu_options">
+                      <button
+                        className="profileVideo_block_menu_option"
+                        onClick={() => handlEdit(item?._id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="profileVideo_block_menu_option"
+                        onClick={() => handleDelete(item?._id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 };
 export default Profile;
