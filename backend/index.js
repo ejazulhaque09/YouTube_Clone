e909bd90c965cc1a57537d4ab69c32f08e5e4d4a
Ejazul Haque <ejazulhaque09@gmail.com>
2026-08-11 22:40:48 +0530

    fix: add frontend url for cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true
 }))
 
